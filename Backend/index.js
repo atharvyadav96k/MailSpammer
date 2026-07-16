65d025c118851c7c37814c8796dd2b86ae158628
Atharv Yadv <arharvyadav96k@gmail.com>
2026-07-16 11:32:44 +0530

    domain mapping

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 
 
 let corsOptions = {
-    origin : true ? ["https://www.fluxmailer.sbs", "https://fluxmailer.sbs"] : ['http://localhost:5500', 'http://127.0.0.1:5500', "http://127.0.0.1:50801"],
+    origin : true ? ["https://www.fluxmailer.sbs", "https://fluxmailer.sbs", "https://fluxmailer-beta.vercel.app"] : ['http://localhost:5500', 'http://127.0.0.1:5500', "http://127.0.0.1:50801"],
     methods: 'GET,POST',
     allowedHeaders: "*"
 }    
